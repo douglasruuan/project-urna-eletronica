39f9527fb83d1b7664fa7b10eede8a7168f82c0a
Your Name <you@example.com>
2020-11-22 23:55:00 -0300

    First Commit

---
+++ etapas.js
@@ -0,0 +1,62 @@
+let etapas = [{
+        titulo: 'Vereador',
+        numeros: 5,
+        candidatos: [{
+                numero: '38111',
+                nome: ' Advogado de God',
+                partido: ' PSL',
+                fotos: [{
+                    url: '38111.jpg',
+                    legenda: 'Vereador',
+                    small: false
+                }]
+            },
+            {
+                numero: '77222',
+                nome: ' Filho da Josefina',
+                partido: ' PTE',
+                fotos: [{
+                    url: '77222.jpg',
+                    legenda: 'Vereador'
+                }]
+            },
+        ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [{
+                numero: '99',
+                nome: ' Mario Gody',
+                partido: ' PSL',
+                vice: 'Ciclos',
+                fotos: [{
+                        url: '99.jpg',
+                        legenda: 'Prefeito'
+                    },
+                    {
+                        url: '99_2.jpg',
+                        legenda: 'Vice-Prefeito',
+                        small: true
+                    }
+                ]
+            },
+            {
+                numero: '84',
+                nome: ' Zulano',
+                partido: ' PSDB',
+                vice: 'Zulo',
+                fotos: [{
+                        url: '84.jpg',
+                        legenda: 'Prefeito'
+                    },
+                    {
+                        url: '84_2.jpg',
+                        legenda: 'Vice-Prefeito',
+                        small: true
+                    }
+                ]
+            },
+        ]
+    }
+];
